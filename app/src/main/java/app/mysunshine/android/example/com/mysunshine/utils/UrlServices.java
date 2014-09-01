febf440414d506c3151f895d9e7ac667b11d0be9
dalekocian <dale.kocian@gmail.com>
2014-08-31 20:59:47 -0500

    Add class to hold urls, and create temp class for future network call.

---
+++ app/src/main/java/app/mysunshine/android/example/com/mysunshine/utils/UrlServices.java
@@ -0,0 +1,12 @@
+package app.mysunshine.android.example.com.mysunshine.utils;
+
+/**
+ * Created by dkocian on 8/31/2014.
+ */
+public class UrlServices {
+    String cityId;
+    String mode;
+    String unitOfMeasurement;
+    int numberOfDays;
+    String s = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&units=metric&cnt=7";
+}
