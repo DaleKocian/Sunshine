e831cc680fc5715a688fece0670d278c8afb8bcd
dalekocian <dale.kocian@gmail.com>
2014-09-09 10:29:17 -0500

    Add map location

--- app/src/main/java/app/mysunshine/android/example/com/mysunshine/MainActivity.java
+++ app/src/main/java/app/mysunshine/android/example/com/mysunshine/MainActivity.java
@@ -1,13 +1,17 @@
 package app.mysunshine.android.example.com.mysunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import app.mysunshine.android.example.com.mysunshine.activities.SettingsActivity;
 import app.mysunshine.android.example.com.mysunshine.fragments.ForecastFragment;
+import app.mysunshine.android.example.com.mysunshine.utils.UrlServices;
 
 public class MainActivity extends ActionBarActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -36,9 +40,20 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            case R.id.action_map:
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+                String location = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+                Uri builtUri = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter(UrlServices.QUERY_PARAM, location).build();
+                intent.setData(builtUri);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/app/mysunshine/android/example/com/mysunshine/fragments/ForecastFragment.java
+++ app/src/main/java/app/mysunshine/android/example/com/mysunshine/fragments/ForecastFragment.java
@@ -18,7 +18,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,7 +28,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import app.mysunshine.android.example.com.mysunshine.R;
 import app.mysunshine.android.example.com.mysunshine.activities.DetailActivity;
@@ -108,6 +112,87 @@ public class ForecastFragment extends Fragment {
     private class FetchWeatherTask extends AsyncTask<String, Integer, String[]> {
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
+        /* The date/time conversion code is going to be moved outside the asynctask later,
+             * so for convenience we're breaking it out into its own method now.
+             */
+        private String getReadableDateString(long time) {
+            // Because the API returns a unix timestamp (measured in seconds),
+            // it must be converted to milliseconds in order to be converted to valid date.
+            Date date = new Date(time * 1000);
+            SimpleDateFormat format = new SimpleDateFormat("E, MMM d");
+            return format.format(date);
+        }
+
+        /**
+         * Prepare the weather high/lows for presentation.
+         */
+        private String formatHighLows(double high, double low) {
+            // Data is fetched in Celsius by default.
+            // If user prefers to see in Fahrenheit, convert the values here.
+            // We do this rather than fetching in Fahrenheit so that the user can
+            // change this option without us having to re-fetch the data once
+            // we start storing the values in a database.
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = sharedPrefs.getString(getString(R.string.pref_units_key), getString(R.string.pref_units_metric));
+            if (unitType.equals(getString(R.string.pref_units_imperial))) {
+                high = (high * 1.8) + 32;
+                low = (low * 1.8) + 32;
+            } else if (!unitType.equals(getString(R.string.pref_units_metric))) {
+                Log.d(LOG_TAG, "Unit type not found: " + unitType);
+            }
+            // For presentation, assume the user doesn't care about tenths of a degree.
+            long roundedHigh = Math.round(high);
+            long roundedLow = Math.round(low);
+            String highLowStr = roundedHigh + "/" + roundedLow;
+            return highLowStr;
+        }
+
+        /**
+         * Take the String representing the complete forecast in JSON Format and
+         * pull out the data we need to construct the Strings needed for the wireframes.
+         * <p/>
+         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
+         * into an Object hierarchy for us.
+         */
+        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+                throws JSONException {
+            // These are the names of the JSON objects that need to be extracted.
+            final String OWM_LIST = "list";
+            final String OWM_WEATHER = "weather";
+            final String OWM_TEMPERATURE = "temp";
+            final String OWM_MAX = "max";
+            final String OWM_MIN = "min";
+            final String OWM_DATETIME = "dt";
+            final String OWM_DESCRIPTION = "main";
+            JSONObject forecastJson = new JSONObject(forecastJsonStr);
+            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+            String[] resultStrs = new String[numDays];
+            for (int i = 0; i < weatherArray.length(); i++) {
+                // For now, using the format "Day, description, hi/low"
+                String day;
+                String description;
+                String highAndLow;
+                // Get the JSON object representing the day
+                JSONObject dayForecast = weatherArray.getJSONObject(i);
+                // The date/time is returned as a long.  We need to convert that
+                // into something human-readable, since most people won't read "1400356800" as
+                // "this saturday".
+                long dateTime = dayForecast.getLong(OWM_DATETIME);
+                day = getReadableDateString(dateTime);
+                // description is in a child array called "weather", which is 1 element long.
+                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                description = weatherObject.getString(OWM_DESCRIPTION);
+                // Temperatures are in a child object called "temp".  Try not to name variables
+                // "temp" when working with temperature.  It confuses everybody.
+                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+                double high = temperatureObject.getDouble(OWM_MAX);
+                double low = temperatureObject.getDouble(OWM_MIN);
+                highAndLow = formatHighLows(high, low);
+                resultStrs[i] = day + " - " + description + " - " + highAndLow;
+            }
+            return resultStrs;
+        }
+
         @Override
         protected String[] doInBackground(String... params) {
             // If there's no zip code, there's nothing to look up.  Verify size of params.
@@ -123,7 +208,7 @@ public class ForecastFragment extends Fragment {
             //
             String zipCode = params[0];
             String format = "json";
-            String units = "metric";
+            String unitType = getString(R.string.pref_units_metric);
             int numDays = 7;
             try {
                 // Construct the URL for the OpenWeatherMap query
@@ -132,7 +217,7 @@ public class ForecastFragment extends Fragment {
                 Uri builtUri = Uri.parse(UrlServices.FORECAST_BASE_URL).buildUpon()
                         .appendQueryParameter(UrlServices.QUERY_PARAM, zipCode)
                         .appendQueryParameter(UrlServices.FORMAT_PARAM, format)
-                        .appendQueryParameter(UrlServices.UNITS_PARAM, units)
+                        .appendQueryParameter(UrlServices.UNITS_PARAM, unitType)
                         .appendQueryParameter(UrlServices.DAYS_PARAM, Integer.toString(numDays))
                         .build();
                 URL url = new URL(builtUri.toString());
